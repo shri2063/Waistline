ce70deb5debafd9b78b483385b9a8c94b389f4c9
DESKTOP-FLJ4OUL\Hi <shrikant@flexli.in>
2023-11-15 18:51:28 +0530

    Added download option and view image option

--- app.py
+++ app.py
@@ -1,37 +1,20 @@
+import base64
+
 import streamlit as st
 import numpy as np
 from streamlit_cropper import st_cropper
-from PIL import Image
+from PIL import Image, ImageOps
 from llm_response import run_llm
 from streamlit_chat import message
-
+from zipfile import ZipFile
+import os
+import cv2
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
-# Upload an image and set some options for demo purposes
-st.header("Cropper Demo")
-img_file = st.sidebar.file_uploader(label='Upload a file', type=['png', 'jpg'])
-realtime_update = st.sidebar.checkbox(label="Update in Real Time", value=True)
-box_color = st.sidebar.color_picker(label="Box Color", value='#0000FF')
-stroke_width = st.sidebar.number_input(label="Box Thickness", value=3, step=1)
 
-aspect_choice = st.sidebar.radio(label="Aspect Ratio", options=["1:1", "16:9", "4:3", "2:3", "Free"])
-aspect_dict = {
-    "1:1": (1, 1),
-    "16:9": (16, 9),
-    "4:3": (4, 3),
-    "2:3": (2, 3),
-    "Free": None
-}
-aspect_ratio = aspect_dict[aspect_choice]
 
-return_type_choice = st.sidebar.radio(label="Return type", options=["Cropped image", "Rect coords"])
-return_type_dict = {
-    "Cropped image": "image",
-    "Rect coords": "box"
-}
-return_type = return_type_dict[return_type_choice]
 
-st.header("Ice breaker Helper Bot")
+st.header("Waist Lyne Functioning Tour")
 # st.session_state.widget = ''
 i = 45
 
@@ -41,7 +24,14 @@ if "chat_answers_history" not in st.session_state:
     st.session_state["chat_answers_history"] = []
 if "issue" not in st.session_state:
     st.session_state.issue = 'sizing'
+# Upload an image and set some options for demo purposes
 
+img_file = st.sidebar.file_uploader(label='Upload a file', type=['png', 'jpg'], key="img_file")
+img_folder = st.sidebar.file_uploader(label = "Upload image collection for sizing", type = "zip", key = "zipfile")
+model = st.sidebar.radio(label="Select Model", options=["blue", "green"], key=4)
+selected_folder = st.sidebar.selectbox("Select a folder: ", ["images"])
+check_images = st.sidebar.button(label = "Check Images")
+download_images = st.sidebar.button(label = "Download Images")
 
 def submit(elseif=None):
     with st.spinner("Generating response...."):
@@ -63,40 +53,83 @@ st.text_input("Prompt", key="widget", placeholder="Enter your prompt here ..", o
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
+
+if img_folder:
+    with open("temp.zip", "wb") as f:
+        f.write(img_folder.read())
+    # Extract all contents of zip folder to a temporary folder
+    with  ZipFile("temp.zip", "r") as zip_ref:
+        zip_ref.extractall("predict")
+    st.success("Folder uploaded and extracted successfully")
+
+
+IMAGE_CHECKED = False
+if check_images:
+    # Display the list of images in the uploaded folder
+    print(selected_folder)
+    directory = "samples/" + selected_folder
+    image_files = [f for f in os.listdir(directory )]
+    for image_file in image_files:
+        image_path = os.path.join(directory, image_file)
+        image = Image.open(image_path)
+        st.image(image, use_column_width=True)
+    IMAGE_CHECKED = True
+    #st.write("List of images in  the uploaded folder:")
+    #print(image_files)
+    #st.write(image_files[0])
+
+if download_images:
+    directory = "samples/" + selected_folder
+    image_files = [f for f in os.listdir(directory)]
+    zip_file_name = f"{selected_folder}_images.zip"
+
+    with st.spinner(f"Creating {zip_file_name}.... "):
+        st.write("Downloading...")
+        with ZipFile(zip_file_name, 'w') as zipf:
+            for image_file in image_files:
+                image_path = os.path.join(directory, image_file)
+                zipf.write(image_path, os.path.basename(image_file))
+        with open(zip_file_name, "rb") as f:
+            zip_contents = f.read()
+
+        # Encode the zip file as base64
+        zip_b64 = base64.b64encode(zip_contents).decode()
+        href = f'<a href="data:application/zip;base64,{zip_b64}" download="{zip_file_name}">Click here to download</a>'
+        st.markdown(href, unsafe_allow_html=True)
+
+
 if img_file:
     img = Image.open(img_file)
-    if not realtime_update:
-        st.write("Double click to save crop")
-    if return_type == 'box':
-        rect = st_cropper(
-            img,
-            realtime_update=realtime_update,
-            box_color=box_color,
-            aspect_ratio=aspect_ratio,
-            return_type=return_type,
-            stroke_width=stroke_width
-        )
-        raw_image = np.asarray(img).astype('uint8')
-        left, top, width, height = tuple(map(int, rect.values()))
-        st.write(rect)
-        masked_image = np.zeros(raw_image.shape, dtype='uint8')
-        masked_image[top:top + height, left:left + width] = raw_image[top:top + height, left:left + width]
-        st.image(Image.fromarray(masked_image), caption='masked image')
-    else:
-        # Get a cropped image from the frontend
-        cropped_img = st_cropper(
-            img,
-            realtime_update=realtime_update,
-            box_color=box_color,
-            aspect_ratio=aspect_ratio,
-            return_type=return_type,
-            stroke_width=stroke_width
-        )
+    img = ImageOps.exif_transpose(img)
+    width, height = img.size
 
-        # Manipulate cropped image at will
-        st.write("Preview")
-        _ = cropped_img.thumbnail((150, 150))
-        st.image(cropped_img)
+    if width > 200.0:
+        new_height = height / width * 200.0
+        new_width = 200.0
+        img = img.resize((int(new_width), int(new_height)))
+
+    elif height > 200.0:
+        new_width = width / height * 200.0
+        new_height = 200.0
+        img = img.resize((int(new_width), int(new_height)))
+
+
+
+    rect = st_cropper(
+        img,
+        realtime_update=True,
+        box_color="blue",
+        aspect_ratio=(1, 1),
+        return_type="box",
+        stroke_width=2
+    )
+    CHEST = []
+    SHOULDER = []
+    TSHIRT = []
+    if st.button('Submit'):
+        print("Session State: " + str(st.session_state))
+
+
 if st.session_state["chat_answers_history"]:
     for generated_response, user_query in zip(st.session_state["chat_answers_history"],
                                               st.session_state["user_prompt_history"]):
