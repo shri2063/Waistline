Corrected top neck point, added error handling to size calculation, sometimes tshirt contour is not appearing properly

sizing/crawler.py
@@ -1,5 +1,6 @@
 from sizing.t_shirt_builder import TShirtBuilder
 from sizing.t_shirt_key_points.LEFT_CHEST_CORNER_PT import LEFT_CHEST_CORNER_PT
+from sizing.t_shirt_key_points.LEFT_COLLAR_PT import LEFT_COLLAR_PT
 from sizing.t_shirt_key_points.LEFT_SHOULDER_PT import *
 import json
 from sizing.t_shirt_key_points.LEFT_WAIST_CORNER_PT import LEFT_WAIST_CORNER_PT
@@ -36,49 +37,49 @@ def build_t_shirt_key_points(predictions):
     mobile_contour = []
     p_x = 0
     p_y = 0
-    try:
-        mobile_class = [x for x in predictions if x.class_ == "mobile"][0]
-        for point in mobile_class.points:
-            mobile_contour.append((point.x, point.y))
-        mobile_contour = np.array(mobile_contour)
+
+    mobile_class = [x for x in predictions if x.class_ == "mobile"][0]
+    for point in mobile_class.points:
+        mobile_contour.append((point.x, point.y))
+    mobile_contour = np.array(mobile_contour)
         #print("mobile")
-        x_coordinates = mobile_contour[:,0]
-        y_coordinates = mobile_contour[:, 1]
-        # Find min and max values along each axis
-        min_x, max_x = np.min(x_coordinates), np.max(x_coordinates)
-        min_y, max_y = np.min(y_coordinates), np.max(y_coordinates)
-        x_avg = (min_x + max_x)/2
-        y_avg = (min_y + max_y) / 2
-        indices = np.where(y_coordinates < y_avg)
-        #print("indices", indices)
-        filtered_top_coordinates = x_coordinates[indices]
-        top_min_x, top_max_x = np.min(filtered_top_coordinates), np.max(filtered_top_coordinates)
-        indices = np.where(y_coordinates > y_avg)
-        filtered_bottom_coordinates = x_coordinates[indices]
-        bot_min_x, bot_max_x = np.min(filtered_bottom_coordinates), np.max(filtered_bottom_coordinates)
-        indices = np.where(x_coordinates < x_avg)
-        filtered_left_coordinates = y_coordinates[indices]
-        left_min_y, left_max_y = np.min(filtered_left_coordinates), np.max(filtered_left_coordinates)
-        indices = np.where(x_coordinates > x_avg)
-        filtered_right_coordinates = y_coordinates[indices]
-        right_min_y, right_max_y = np.min(filtered_right_coordinates), np.max(filtered_right_coordinates)
+    x_coordinates = mobile_contour[:,0]
+    y_coordinates = mobile_contour[:, 1]
+    # Find min and max values along each axis
+    min_x, max_x = np.min(x_coordinates), np.max(x_coordinates)
+    min_y, max_y = np.min(y_coordinates), np.max(y_coordinates)
+    x_avg = (min_x + max_x)/2
+    y_avg = (min_y + max_y) / 2
+    indices = np.where(y_coordinates < y_avg)
+    #print("indices", indices)
+    filtered_top_coordinates = x_coordinates[indices]
+    top_min_x, top_max_x = np.min(filtered_top_coordinates), np.max(filtered_top_coordinates)
+    indices = np.where(y_coordinates > y_avg)
+    filtered_bottom_coordinates = x_coordinates[indices]
+    bot_min_x, bot_max_x = np.min(filtered_bottom_coordinates), np.max(filtered_bottom_coordinates)
+    indices = np.where(x_coordinates < x_avg)
+    filtered_left_coordinates = y_coordinates[indices]
+    left_min_y, left_max_y = np.min(filtered_left_coordinates), np.max(filtered_left_coordinates)
+    indices = np.where(x_coordinates > x_avg)
+    filtered_right_coordinates = y_coordinates[indices]
+    right_min_y, right_max_y = np.min(filtered_right_coordinates), np.max(filtered_right_coordinates)
 
-        print("Top X:", top_min_x, top_max_x)
-        print("Bot X:", bot_min_x, bot_max_x)
-        print("Left Y:",left_min_y, left_max_y )
-        print("Right Y:", right_min_y, right_max_y)
-        p_x = (((top_max_x - top_min_x) + (bot_max_x - bot_min_x))/2)/MOBILE_WIDTH
-        p_y = (((left_max_y - left_min_y) + (right_max_y - right_min_y)) / 2)/MOBILE_LENGTH
-        p_y = p_y*0.95
-        if p_x > 1.2*p_y:
-            p_x = 0.95*p_x
+    print("Top X:", top_min_x, top_max_x)
+    print("Bot X:", bot_min_x, bot_max_x)
+    print("Left Y:",left_min_y, left_max_y )
+    print("Right Y:", right_min_y, right_max_y)
+    p_x = (((top_max_x - top_min_x) + (bot_max_x - bot_min_x))/2)/MOBILE_WIDTH
+    p_y = (((left_max_y - left_min_y) + (right_max_y - right_min_y)) / 2)/MOBILE_LENGTH
+    p_y = p_y*0.95
+    if p_x > 1.2*p_y:
+         p_x = 0.95*p_x
 
-    except Exception as e:
-        print(e.__str__())
+
 
     LEFT_WAIST_CORNER_PT(predictions, t_shirt_builder)
     LEFT_CHEST_CORNER_PT(predictions, t_shirt_builder)
     LEFT_SHOULDER_PT(predictions, t_shirt_builder, t_shirt_builder.LEFT_CHEST_CORNER_PT.border_contour_index + 1)
+    LEFT_COLLAR_PT(predictions, t_shirt_builder, t_shirt_builder.LEFT_SHOULDER_PT.border_contour_index + 1)
     RIGHT_SHOULDER_PT(predictions, t_shirt_builder, t_shirt_builder.LEFT_SHOULDER_PT.border_contour_index + 1)
     RIGHT_CHEST_CORNER_PT(predictions,  t_shirt_builder , t_shirt_builder.RIGHT_SHOULDER_PT.border_contour_index + 1)
     RIGHT_WAIST_CORNER_PT(predictions,  t_shirt_builder , t_shirt_builder.RIGHT_CHEST_CORNER_PT.border_contour_index + 1)
@@ -99,7 +100,7 @@ def build_t_shirt_key_points(predictions):
 
     y_coords = [coord[1] for coord in coords]
     average_y_waist = sum(y_coords) / len(y_coords)
-    y_neck = t_shirt_class.corner_coordinate.top_coordinate[1]
+    y_neck = t_shirt_builder.LEFT_COLLAR_PT.coordinates[1]
     plt.scatter(int(t_shirt_class.corner_coordinate.top_coordinate[0]), int(t_shirt_class.corner_coordinate.top_coordinate[1]), c='black',
                marker='o', s=100, label='Changed Point')
     plt.savefig('sizing\scatter_plot.png')
@@ -130,61 +131,4 @@ def build_t_shirt_key_points(predictions):
 
 
 
-class Crawler:
 
-
-    def __init__(self, border_contour, t_shirt_builder, image, border_contour_1):
-        self.border_contour = border_contour
-        self.t_shirt_builder = t_shirt_builder
-        self.read_image = cv2.imread(image)
-        self.polygon_cp = border_contour_1
-
-    def set_attribute(self, variable, value):
-        setattr(self.t_shirt_builder, variable, value)
-
-    def get_image(self):
-        grid_interval = 30
-        grid_colour = (0 ,255 ,0)
-        # for y in range(0, self.read_image.shape[0], grid_interval):
-         #  cv2.line(self.read_image, (0,y), (self.read_image.shape[1],y),grid_colour,1)
-        # for x in range(0, self.read_image.shape[1], grid_interval):
-         #  cv2.line(self.read_image, (x,0), (x, self.read_image.shape[0]),grid_colour,1)
-
-        # cv2.imwrite("image_with_grid.jpg",self.read_image)
-
-        plt.imshow(self.read_image)
-        plt.show()
-
-        # cv2.imshow("Marked Points", self.read_image)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-
-
-
-    def find_slope(self, coordinates, origin):
-        # print(coordinates)
-        # print(origin)
-        return abs(coordinates[1] - origin[1]) / abs(coordinates[0] - origin[0])
-
-
-    def calculate_length(self):
-        length_between_points = 0
-        print("First point on border contour", tuple(self.border_contour[0][0]), self.border_contour.shape)
-        outer_point_index = 0
-        inner_point_index = 0
-        self.get_left_sleeve_outside_pt()
-        self.get_all_corner_points_using_polygon_appx()
-        print("sleeve inside point", self.left_sleeve_inside_pt)
-        print("sleeve outside point", self.left_sleeve_outside_pt)
-
-        for index, point in enumerate(self.border_contour):
-            if tuple(self.border_contour[index][0]) == self.left_sleeve_outside_pt:
-                outer_point_index = index
-                break
-        for index, point in enumerate(self.border_contour):
-            if tuple(self.border_contour[index][0]) == self.left_sleeve_inside_pt:
-                inner_point_index = index
-                break
-
-        # print("left sleeve outside point", left_sleeve_outside_point)
-        print("sleeve length ", cv2.arcLength(self.border_contour[outer_point_index:inner_point_index], False))

sizing/t_shirt_key_points/LEFT_COLLAR_PT.py
@@ -1,26 +1,30 @@
-import cv2
 import numpy as np
-from decimal  import  Decimal
+import matplotlib.pyplot as plt
+def LEFT_COLLAR_PT(predictions, t_shirt_builder, mIndex):
+    t_shirt_contour = []
+    t_shirt = [x for x in predictions if x.class_ == "t_shirt"][0]
+    for point in t_shirt.points:
+        t_shirt_contour.append((point.x, point.y))
+    t_shirt_contour = np.array(t_shirt_contour)
 
 
-def LEFT_COLLAR_PT(border_contour, read_image, t_shirt_builder, index):
-    reshaped_contour = border_contour.reshape(border_contour.shape[0], 2)
+    for fIndex, contour in enumerate(t_shirt_contour):
+        if fIndex > mIndex:
+            slope = abs((t_shirt_contour[fIndex + 5][1] - t_shirt_contour[fIndex][1]) / (
+                    t_shirt_contour[fIndex + 5][0] - t_shirt_contour[fIndex][0]))
 
-    while True:
-        if border_contour[index%(len(border_contour) - 1)][1] >= border_contour[(index + 1)%(len(border_contour) - 1)][1]:
-            while True:
-                if border_contour[index][1] <\
-                        border_contour[(index + 1)][1]:
-                    coordinates = tuple(reshaped_contour[index% (len(border_contour))])
-                    t_shirt_builder.LEFT_COLLAR_PT = t_shirt_builder.LEFT_COLLAR_PT._replace(
-                        coordinates=coordinates,  border_contour_index=index
-                    )
-                    break
-                else:
-                    index = (index + 1)% (len(border_contour))
-
-            break
-
-        else:
-            index = (index + 1)%(len(border_contour) - 1)
-    cv2.circle(read_image, tuple(coordinates), 25, (255, 0, 0), 3)
+            if t_shirt_contour[fIndex + 1][1] <= t_shirt_contour[fIndex][1] and  slope < 1.8:
+                #print(slope)
+                continue
+            else:
+                print("Found Left Collar")
+                print(contour[0], contour[1])
+                #print(slope)
+                plt.scatter(int(t_shirt_contour[fIndex][0]), int(t_shirt_contour[fIndex][1]), c='black',
+                         marker='o', s=100, label='Changed Point')
+                t_shirt_builder.LEFT_COLLAR_PT = t_shirt_builder.LEFT_COLLAR_PT._replace(
+                    coordinates=(int(t_shirt_contour[fIndex][0]), int(t_shirt_contour[fIndex][1])),
+                    border_contour_index=fIndex - 1
+                )
+                break
+    plt.savefig('sizing\scatter_plot.png')

sizing/yolo_model_prediction.py
@@ -28,49 +28,57 @@ class ImageData(BaseModel):
 # return project.version(1).model
 
 def get_prediction_using_YOLO(filename: str):
-    model = YOLO("trained_models/best.pt")
-    results = model.predict(source=filename, conf=0.25)
+    model = YOLO("trained_models/best_1.pt")
+    results = model.predict(source=filename, conf=0.1)
     masks = results[0].masks.cpu()
-    # print(masks.data)
-    for mask in masks.data:
-        # print(mask)
-        contours = measure.find_contours(np.array(mask), 0.5)
-        contour_coordinates = [contour for contour in contours]
-        points = [{"x": x, "y": y} for y, x in contour_coordinates[0]]
-        # print(points)
+
 
     boxes = results[0].boxes
-    # print(np.array(boxes[0].xyxy)[0,0])
+    #print(boxes)
     classes = np.array(boxes.cls)
+    #print(classes)
     cnf = boxes.conf
+    #print(cnf)
     predictions_data = PredictionsData(predictions=[])
 
     i = 0
+    #print(masks.xy[0])
     for mask in masks.data:
+
         contours = measure.find_contours(np.array(mask), 0.5)
         contour_coordinates = [contour for contour in contours]
         points = [{"x": x, "y": y} for y, x in contour_coordinates[0]]
+
+
+
+
         prediction_item = PredictionItem(x=np.array(boxes[i].xyxy)[0, 0], y=np.array(boxes[i].xyxy)[0, 1],
                                          width=np.array(boxes[i].xyxy)[0, 2],
                                          height=np.array(boxes[i].xyxy)[0, 3],
                                          confidence=cnf[i], class_id=classes[i],
                                          points=points, class_="bla")
         # print(prediction_item)
+
         predictions_data.predictions.append(prediction_item)
         i = i + 1
+    #neck_item_with_max_cnf = max((prediction_item for prediction_item in predictions_data.predictions if prediction_item.class_id == 2), key = lambda x: x.confidence,default=None)
+    #predictions_data.predictions = [prediction_item for prediction_item in predictions_data.predictions if prediction_item.class_id != 2
+    #                                 or(prediction_item.class_id == 2 and prediction_item.confidence == neck_item_with_max_cnf.confidence)]
+    #for item in   predictions_data.predictions:
+    #    print(item.class_id)
+        #print(predictions_data)
         # print(i)
     # Convert the JSON object to a JSON string
     # Convert the PredictionsData object to a dictionary
-    predictions_data_dict = predictions_data.dict()
-    json_string = json.dumps(predictions_data_dict, indent=4)
-    #file_path = 'sizing/t_shirt.txt'
-    #with open(file_path, 'w') as file:
-        #file.write(json_string)
+
+    # file_path = 'sizing/t_shirt.txt'
+    # with open(file_path, 'w') as file:
+    # file.write(json_string)
     return predictions_data
 
 
 def model_json_prediction_for_sizing_issue(filename: str) -> any:
-    # model_prediction = model.green_tshirt(filename, confidence=20).json()
+    # model_prediction = model.clean_tshirts(filename, confidence=20).json()
     # print(model_prediction)
     # file_path = "D:\Desktop\ice-breaker\intro-to-vector\data.txt"
     # with open(file_path, "w") as file:
@@ -96,10 +104,16 @@ def model_json_prediction_for_sizing_issue(filename: str) -> any:
                 if prediction.class_ == "t_shirt":
                     x.append(int(point.x))
                     y.append(int(point.y))
+                if prediction.class_ == "mobile":
+                    x.append(int(point.x))
+                    y.append(int(point.y))
                     t_shirt_contour.append((point.x, point.y))
 
-    # Create a scatter plot
-    t_shirt_contour = np.array(t_shirt_contour)
+                if "sleeve" in prediction.class_:
+                    x.append(int(point.x))
+                    y.append(int(point.y))
+                    t_shirt_contour.append((point.x, point.y))
+
     print("Plotting scatter plot")
     plt.scatter(x, y)
 
@@ -109,8 +123,8 @@ def model_json_prediction_for_sizing_issue(filename: str) -> any:
     plt.title('Scatter Plot')
 
     # Show the plot
-    #plt.show()
-    #plt.savefig('sizing\scatter_plot.png')
+    # plt.show()
+    # plt.savefig('sizing\scatter_plot.png')
 
     # build_t_shirt_key_points(t_shirt_contour, "sizing_img.jpg")
     # for prediction in predictions: