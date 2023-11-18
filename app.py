1af2d58662b7eba46fbb3150823f7fe102ea3643
bhole <shrikant@flexli.in>
2023-11-18 16:11:26 +0530

    Made modification to examples in LLM

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from streamlit_chat import message
 import os
 
 from llm.llm_response import ai_introduction, run_llm, chatgpt_call_with_memory, messages
-from llm.llm_sizing import find_sizing_category_for_issue
+from llm.llm_sizing import generate_sizing_category_for_issue
 from models.box import Box
 from sizing.crawler import build_t_shirt_key_points
 from sizing.yolo_model_prediction import model_json_prediction_for_sizing_issue
@@ -28,44 +28,96 @@ if "user_prompt_history" not in st.session_state:
 if "chat_answers_history" not in st.session_state:
     st.session_state["chat_answers_history"] = []
 if "issue" not in st.session_state:
-    st.session_state.issue = 'quality'
-if "generated_issue" not in st.session_state:
-    st.session_state.generated_issue = ''
-GENERATED_ISSUE: str = ''
+    st.session_state.issue = ''
+if "sizing_category" not in st.session_state:
+    st.session_state.sizing_category = ''
+if "image_uploaded" not in st.session_state:
+    st.session_state.image_uploaded = False
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+img_file = None
+
 # Upload an image and set some options for demo purposes
 
-img_file = st.sidebar.file_uploader(label='Upload a file', type=['png', 'jpg'], key="img_file")
-img_folder = st.sidebar.file_uploader(label = "Upload image collection for sizing", type = "zip", key = "zipfile")
+# img_file = st.sidebar.file_uploader(label='Upload a file', type=['png', 'jpg'], key="img_file")
+img_folder = st.sidebar.file_uploader(label="Upload image collection for sizing", type="zip", key="zipfile")
 model = st.sidebar.radio(label="Select Model", options=["blue", "green"], key="model")
 defect = st.sidebar.radio(label="Select defect", options=["quality", "sizing"], key="defect")
 selected_folder = st.sidebar.selectbox("Select a folder: ", ["clean_tshirts", "black_tshirt", "green_tshirt"])
-check_images = st.sidebar.button(label = "Check Images")
-download_images = st.sidebar.button(label = "Download Images")
+check_images = st.sidebar.button(label="Check Images")
+download_images = st.sidebar.button(label="Download Images")
+# Initial Message
 message(ai_introduction, key=i.__str__())
+
 
 def submit(elseif=None):
     with st.spinner("Generating response...."):
         print("Session State " + str(st.session_state))
         cust_query = st.session_state.widget
+        print(cust_query)
         generated_response = run_llm(cust_query, fetch_conversation_till_now())
-
-        global  GENERATED_ISSUE
-        st.session_state.generated_issue = str(generated_response)
-        print("Generated Issue", GENERATED_ISSUE)
-        print(generated_response)
-        if "Sizing Issue" in str(generated_response):
-            st.session_state.issue = 'sizing'
-            print("Session State: " + str(st.session_state.issue))
-        elif "Quality Issue" in str(generated_response):
-            st.session_state.issue = 'quality'
-            print("Session State: " + str(st.session_state.issue))
-
         st.session_state["user_prompt_history"].append(cust_query)
-        st.session_state["chat_answers_history"].append(generated_response)
+        processed_response = revise_user_state_based_upon_response(str(generated_response))
+        print(processed_response)
+        st.session_state["chat_answers_history"].append(processed_response)
 
+
+if st.session_state["chat_answers_history"]:
+    for generated_response, user_query in zip(st.session_state["chat_answers_history"],
+                                              st.session_state["user_prompt_history"]):
+        i = i + 1
+        message(user_query, is_user=True, key=i.__str__())
+        i = i + 1
+        message(generated_response, key=i.__str__())
+
+
+if st.session_state.issue != '' and st.session_state.image_uploaded == False:
+    sample_image = Image.open("sizing/sample_sizing_image.jpg")
+    st.image(sample_image, caption="Sample Image", width=300)
+    img_file = st.file_uploader(label='Upload a file', type=['png', 'jpg'], key="img_file")
 st.text_input("Prompt", key="widget", placeholder="Enter your prompt here ..", on_change=submit)
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
+def revise_user_state_based_upon_response(generated_response):
+    if st.session_state.issue == '':
+        if "Issue:Sizing" in str(generated_response):
+            st.session_state.issue = 'sizing'
+            print("Session State: " + str(st.session_state.issue))
+            category = get_sizing_category_for_issue(generated_response)
+            st.session_state.sizing_category = category
+            print("Category", category)
+            if category == "0":
+                st.session_state.issue = ''
+
+                return ("I am really sorry. I understand you have issue in sizing related to tshirt."
+                        "But I am not able to clearly understand the exact issue. If you don't mind can you"
+                        "explain a little more in detail")
+            else:
+
+                return generated_response + (". To help you further I will need you to take photo of the tshirt"
+                                             "You need to turn the tshirt inside out and then hold your phone over the top of image. Just like the one I am "
+                                             "attaching in the image"
+                                             "If you don't have a tshirt, you can pick a tshirt with relevent issue from our gallery")
+
+
+
+        elif "Issue:Quality" in str(generated_response):
+            st.session_state.issue = 'quality'
+            print("Session State: " + str(st.session_state.issue))
+        return generated_response
+    return generated_response
+
+
+def get_sizing_category_for_issue(gernerated_response):
+    split_result = str(gernerated_response).split("Noting down")
+    print(split_result)
+    issue = ''
+    # Check if there's a second part (index 1) after the split
+    if len(split_result) > 1:
+        # Extract the string after "Issue:"
+        issue = split_result[1].strip()
+
+    print("Issue", issue)
+    return generate_sizing_category_for_issue(issue)
 
 
 if img_folder:
@@ -76,21 +128,20 @@ if img_folder:
         zip_ref.extractall("predict")
     st.success("Folder uploaded and extracted successfully")
 
-
 IMAGE_CHECKED = False
 if check_images:
     # Display the list of clean_tshirts in the uploaded folder
     print(selected_folder)
     directory = "samples/" + selected_folder
-    image_files = [f for f in os.listdir(directory )]
+    image_files = [f for f in os.listdir(directory)]
     for image_file in image_files:
         image_path = os.path.join(directory, image_file)
         image = Image.open(image_path)
         st.image(image, use_column_width=True)
     IMAGE_CHECKED = True
-    #st.write("List of clean_tshirts in  the uploaded folder:")
-    #print(image_files)
-    #st.write(image_files[0])
+    # st.write("List of clean_tshirts in  the uploaded folder:")
+    # print(image_files)
+    # st.write(image_files[0])
 
 if download_images:
     directory = "samples/" + selected_folder
@@ -112,7 +163,16 @@ if download_images:
         st.markdown(href, unsafe_allow_html=True)
 
 
-if img_file:
+def fetch_conversation_till_now():
+    conversation = []
+    for generated_response, user_query in zip(st.session_state["chat_answers_history"],
+                                              st.session_state["user_prompt_history"]):
+        new_query = {"query": user_query, "answer": generated_response}
+        conversation.append(new_query)
+        return conversation
+
+
+if img_file is not None:
 
     img = Image.open(img_file)
     img = ImageOps.exif_transpose(img)
@@ -127,10 +187,6 @@ if img_file:
         new_width = width / height * 200.0
         new_height = 200.0
         img = img.resize((int(new_width), int(new_height)))
-
-
-
-
 
     rect = st_cropper(
         img,
@@ -151,10 +207,10 @@ if img_file:
             image_files = [f for f in os.listdir(directory)]
             for image_file in image_files:
                 print(image_file)
-                #img = Image.open(os.path.join(directory, image_file))
-                #raw_image = np.asarray(img).astype('uint8')
-                #bgr_image = cv2.cvtColor(raw_image, cv2.COLOR_RGB2BGR)
-                #cv2.imwrite('sizing_img.jpg', bgr_image)
+                # img = Image.open(os.path.join(directory, image_file))
+                # raw_image = np.asarray(img).astype('uint8')
+                # bgr_image = cv2.cvtColor(raw_image, cv2.COLOR_RGB2BGR)
+                # cv2.imwrite('sizing_img.jpg', bgr_image)
                 predictions = model_json_prediction_for_sizing_issue(os.path.join(directory, image_file))
                 predictions = get_corner_coordinates_for_tshirt(predictions)
                 corrected_predictions = correct_class_for_sleeves(predictions)
@@ -171,7 +227,8 @@ if img_file:
             print(f"chest: " + str(sum(CHEST) / len(CHEST)))
             print(f"shoulder: " + str(sum(SHOULDER) / len(SHOULDER)))
             print(f"tshirt: " + str(sum(TSHIRT) / len(TSHIRT)))
-            st.write('chest: ' + str(sum(CHEST) / len(CHEST)) + 'shoulder: ' +  str(sum(SHOULDER) / len(SHOULDER)) + "tshirt: " + str(sum(TSHIRT) / len(TSHIRT)))
+            st.write('chest: ' + str(sum(CHEST) / len(CHEST)) + 'shoulder: ' + str(
+                sum(SHOULDER) / len(SHOULDER)) + "tshirt: " + str(sum(TSHIRT) / len(TSHIRT)))
 
         if st.session_state.issue == "quality":
             print("Session State: " + str(st.session_state))
@@ -179,7 +236,7 @@ if img_file:
             st.session_state["chat_answers_history"] = []
             st.write('We are working on your query. Please wait.')
 
-            #raw_image = np.asarray(img).astype('uint8')
+            # raw_image = np.asarray(img).astype('uint8')
             left, top, width, height = tuple(map(int, rect.values()))
             input_box = Box(
                 x=left,
@@ -216,29 +273,6 @@ if img_file:
             generated_response = "Apologies for my earlier reply. " + generated_response
         message(generated_response, key=i.__str__())
 
-if st.button('Summarize'):
-    #messages.append({'role': 'user', 'content': 'Please summarize the discussion till now in format and structure ,so I can directly pass it as a prefix for my next prompt with no other context required  '})
-    #response = chatgpt_call_with_memory(messages)
-    # Split the string based on "Issue:"
-    #GENERATED_ISSUE = "Thanks . I understand that you find tshirt chest size too long, but can you exaplain in detail if tshirt chest is wider or longer than you expected. Noting down the Issue:Sizing:chest size is longer than expected "
-
-
-    print(st.session_state.generated_issue)
-    split_result = str(st.session_state.generated_issue).split("Noting down")
-    print(split_result)
-    issue = ''
-    # Check if there's a second part (index 1) after the split
-    if len(split_result) > 1:
-        # Extract the string after "Issue:"
-        issue = split_result[1].strip()
-
-    print("Issue", issue)
-    response = find_sizing_category_for_issue(issue)
-    st.write(response)
-
-
-
-
 if st.button('Check result'):
     if model == "blue":
         model = yolo_chirag()
@@ -250,19 +284,3 @@ if st.button('Check result'):
     predicted_image = Image.open(predicted_image_file)
     predicted_image = np.asarray(predicted_image).astype('uint8')
     st.image(Image.fromarray(predicted_image), caption='Predicted Image')
-
-if st.session_state["chat_answers_history"]:
-    for generated_response, user_query in zip(st.session_state["chat_answers_history"],
-                                              st.session_state["user_prompt_history"]):
-        i = i + 1
-        message(user_query, is_user=True, key=i.__str__())
-        i = i + 1
-        message(generated_response, key=i.__str__())
-
-def fetch_conversation_till_now():
-    conversation = []
-    for generated_response, user_query in zip(st.session_state["chat_answers_history"],
-                                              st.session_state["user_prompt_history"]):
-        new_query = {"query": user_query, "answer": generated_response}
-        conversation.append(new_query)
-        return conversation
--- llm/llm_sizing.py
+++ llm/llm_sizing.py
@@ -1,33 +1,50 @@
 k = "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"
 from langchain.llms import OpenAI
 from langchain import PromptTemplate
+CATEGORY_LIST = ["I don't know","Category 1","Category 2","Category 3",
+                 "Category 4","Category 5","Category 6","Category 7",
+                 "Category 8","Category 9", "Category 10", "Category 11"]
+CATEGORY_DICT = {
+    0: "I don't know",
+    1: "Involves neck or sleeves",
+    2: "Chest size is smaller",
+    3: "Chest size is larger",
+    4: "Shoulder size is larger",
+    5: "Shoulder size is smaller",
+    6: "Waist size is larger",
+    7: "Waist size is smaller",
+    8: "tshirt length is long",
+    9: "tshirt length is short",
+    10: "tshirt in general undersize",
+    11: "tshirt in general oversize"
+}
 template = """Answer the question based on the context below. You have to select correct Issue Category for 
 the User query provided in the Input query. In the context below you have been provided multiple Issue categories along with 
 multiple examples for each issue category. In your Answer just mention the Category name, for example - Category 1. Please note you have to select only from the given categories. Strictly do not improvise or innovate new  categories If the category  cannot be decided using the information provided answer
 with "I don't know"
 
 Context: 
-Category 0 -  Involves neck or sleeves. Examples for this category: neck opening too tight, neck opening
+Category 1 -  Involves neck or sleeves. Examples for this category: neck opening too tight, neck opening
  too wide, sleeves are too short, sleeves are too long, length of both sleeves do not match
-Category 1- Chest size is smaller. Examples for this category: tshirt tight as chest, not fitting 
+Category 2- Chest size is smaller. Examples for this category: tshirt tight as chest, not fitting 
 near chest,size smaller for chest, cannot breathe because too tight.  
-Category 2- Chest size is larger. Examples for this category: tshirt loose at chest, loose fitting 
+Category 3- Chest size is larger. Examples for this category: tshirt loose at chest, loose fitting 
 near chest,size larger for chest,tshirt wide near chest.
-Category 3- Shoulder size is larger. Examples for this category: tshirt loose at shoulder, loose fitting 
+Category 4- Shoulder size is larger. Examples for this category: tshirt loose at shoulder, loose fitting 
 near shoulder,shoulders are dropping down,tshirt wide near shoulder.
-Category 4-  Shoulder size is smaller. Examples for this category: tshirt tight at shoulder, tight fitting 
+Category 5-  Shoulder size is smaller. Examples for this category: tshirt tight at shoulder, tight fitting 
 near shoulder,size smaller for shoulder,tshirt underfitting near shoulder.
-Category 5- Waist size is larger. Examples for this category: tshirt loose at stomach, loose fitting 
+Category 6- Waist size is larger. Examples for this category: tshirt loose at stomach, loose fitting 
 near waist,tshirt appears oversized at waist or stomach,tshirt wide near waist.
-Category 6-  Waist size is smaller. Examples for this category: tshirt tight at stomach, stomach is clearly visible
+Category 7-  Waist size is smaller. Examples for this category: tshirt tight at stomach, stomach is clearly visible
 ,size smaller for waist,tshirt underfitting near waist or stomach.
-Category 7- tshirt length is long. Examples for this category: tshirt too long, tshirt going below waist
+Category 8- tshirt length is long. Examples for this category: tshirt too long, tshirt going below waist
 tshirt length oversized,tshirt dropping down,tshirt wide in length.
-Category 8-  tshirt length is short. Examples for this category: tshirt too short, tight not covering below waist 
+Category 9-  tshirt length is short. Examples for this category: tshirt too short, tight not covering below waist 
 ,size smaller in length,tshirt underfitting in length.
-Category 9-  tshirt in general undersize. Examples for this category: tshirt size should be L but it is M, tshirt tight fitting 
+Category 10-  tshirt in general undersize. Examples for this category: tshirt size is S but expected is M, tshirt size is M but expected is L, tshirt tight fitting 
 ,size smaller for tshirt,tshirt underfitting.
-Category 10-  tshirt in general oversize. Examples for this category: tshirt size should be M but it is L, tshirt loose fitting 
+Category 11-  tshirt in general oversize. Examples for this category: tshirt size is L but expected is M, tshirt size is M but expected is S, tshirt loose fitting 
 ,size larger for tshirt,tshirt overfitting..
 
  
@@ -42,7 +59,11 @@ openai_llm = OpenAI(
     openai_api_key=k
 )
 #encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
-def find_sizing_category_for_issue(query: str):
+def generate_sizing_category_for_issue(query: str):
     prompt_template = PromptTemplate(input_variables=["query"], template=template)
     response = openai_llm(prompt_template.format(query = query))
-    return response
+    print("Category response", str(response))
+    for index, category in enumerate(CATEGORY_LIST):
+        if category in response:
+            return str(index)
+    return str(0)
