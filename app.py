b8557803a6c3b8ecbb2fe29dfe7b8d6e298f7846
bhole <shrikant@flexli.in>
2023-11-19 18:31:59 +0530

    Made modification to examples in LLM

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from sizing.sizing_pre_processing import correct_class_for_sleeves, get_corner_c
 from quality.roboflow_inference import model_img_prediction, generate_response_based_upon_result, \
     get_iou_input_and_iou_predicted, yolo_chirag
 
-st.header("WaistLyne v1.1")
+st.header("WaistLyne v1.2")
 # st.session_state.widget = ''
 i = 45
 
@@ -36,7 +36,7 @@ if "generated_issue" not in st.session_state:
     st.session_state.generated_issue = ''
 if "sizing_category" not in st.session_state:
     st.session_state.sizing_category = ''
-if "image_uploaded" not in st.session_state:
+if "sizing_fist_ref" not in st.session_state:
     st.session_state.sizing_fist_ref = False
 if "t_shirt_size" not in st.session_state:
     st.session_state.t_shirt_size = ''
@@ -67,17 +67,82 @@ message(ai_introduction, key=i.__str__())
 if st.session_state["chat_messages"]:
     for chat_message in st.session_state["chat_messages"]:
         i = i + 1
+
         if chat_message["is_user"]:
             message(chat_message["message"], is_user=True, key=i.__str__())
         else:
             message(chat_message["message"], key=i.__str__())
 
-st.session_state.issue_category = 'sizing'
-st.session_state.generated_issue = 'tshirt is oversized'
-st.session_state.sizing_fist_ref = True
-st.session_state.t_shirt_size = "M"
+
+
+#st.session_state.issue_category = 'sizing'
+if st.session_state.issue_category == 'sizing' and st.session_state.sizing_fist_ref == False:
+    sample_image = Image.open("sizing/sample_sizing_image.jpg")
+    st.image(sample_image, caption="Sample Image", width=300)
+    sizing_img = st.file_uploader(label='Upload Image of your tshirt', type=['png', 'jpg'], key="img_file")
+    st.session_state.t_shirt_size = st.text_input("Please enter size  of your tshirt in Capital. Example: L")
+
+    if sizing_img:
+        sizing_img = Image.open(sizing_img)
+        sizing_img.save('sizing/sizing_img.jpg')
+
+    if st.button('Submit Image'):
+        if  st.session_state.t_shirt_size not in ('XS', 'S', 'M', 'L', 'XL', '2XL', '3XL', '4XL'):
+            st.write("Tshirt size is not entered properly. Please select between XS,S,M,L,XL,2XL,3XL,4XL ")
+        if sizing_img is None:
+            st.write("please upload an image")
+        predictions = model_json_prediction_for_sizing_issue('sizing/sizing_img.jpg')
+        predictions = get_corner_coordinates_for_tshirt(predictions)
+        corrected_predictions = correct_class_for_sleeves(predictions)
+        context = 'Here is the information available for a tshirt regarding its sizing. '
+        tshirt_size = st.session_state.t_shirt_size
+        try:
+            CbyS, CbyL, SbyL = get_ratios_for_tshirt(corrected_predictions)
+            if t_shirt_size_chart_ratio[tshirt_size]['C/S'][0] <= CbyS <= t_shirt_size_chart_ratio[tshirt_size]['C/S'][1]:
+                context += "Chest by Shoulder ratio appears to be within range. "
+            elif CbyS < t_shirt_size_chart_ratio[tshirt_size]['C/S'][0]:
+                context += ("Chest by Shoulder ratio is less than expected. It means either Chest is of smaller size. "
+                            "or Shoulder is of larger size. ")
+            elif CbyS > t_shirt_size_chart_ratio[tshirt_size]['C/S'][1]:
+                context += ("Chest by Shoulder ratio is greater than expected. It means either Chest is "
+                            "of larger  size or Shoulder is of smaller size. ")
+
+            if t_shirt_size_chart_ratio[tshirt_size]['C/L'][0] <= CbyL <= t_shirt_size_chart_ratio[tshirt_size]['C/L'][1]:
+                context += "Chest by Tshirt length ratio appears to be within range. "
+            elif CbyL < t_shirt_size_chart_ratio[tshirt_size]['C/L'][0]:
+                context += ("Chest by Tshirt length ratio is less than expected. It means either Chest is of smaller "
+                            "size or Tshirt length is of larger size. ")
+            elif CbyL > t_shirt_size_chart_ratio[tshirt_size]['C/L'][1]:
+                context += ("Chest by Tshirt length ratio is greater than expected. It means either Chest is "
+                            "of larger  size or Tshirt length is of smaller size. ")
+
+            if t_shirt_size_chart_ratio[tshirt_size]['S/L'][0] <= SbyL <= t_shirt_size_chart_ratio[tshirt_size]['S/L'][1]:
+                context += "Shoulder by Tshirt length ratio appears to be within range. "
+            elif SbyL < t_shirt_size_chart_ratio[tshirt_size]['S/L'][0]:
+                context += (
+                    "Shoulder by Tshirt length ratio is less than expected. It means either Shoulder is of smaller "
+                    "size or Tshirt length is of larger size. ")
+            elif SbyL > t_shirt_size_chart_ratio[tshirt_size]['S/L'][1]:
+                context += ("Shoulder by Tshirt length ratio is greater than expected. It means either Shoulder is "
+                            "of larger  size or Tshirt length is of smaller size. ")
+
+            # st.write(context)
+            # st.write(st.session_state.generated_issue)
+
+            response = generate_response_based_upon_sizing_calculations(st.session_state.generated_issue, context)
+            st.session_state.sizing_fist_ref = True
+            response = response + (" However, if you want me to get precise calculation. I would like to "
+                                   "have multiple images of the same tshirt in a zip folder.Also I need to have "
+                                   "reference of any mobile to be placed on the image to be used as a reference ")
+            # st.write(response)
+            i = i + 1
+            st.session_state["chat_messages"].append({"is_user": False, "message": response})
+            message(response, key=i.__str__())
+        except Exception as e:
+            print(e.__str__())
+
+print("sizing_fist_ref", st.session_state.sizing_fist_ref)
 if st.session_state.issue_category == 'sizing' and st.session_state.sizing_fist_ref == True:
-    st.write("hello")
     sample_image = Image.open("sizing/sample_sizing_img_w_mobile.jpeg")
     st.image(sample_image, caption="Sample Image", width=300)
     sizing_folder = st.file_uploader(label="Upload images zip  for sizing", type="zip", key="zipfile1")
@@ -151,73 +216,9 @@ if st.session_state.issue_category == 'sizing' and st.session_state.sizing_fist_
                                                                                 context)
                 i = i + 1
                 message(response, key=i.__str__())
+                st.session_state["chat_messages"].append({"is_user": False, "message": response})
 
 
-
-
-
-if st.session_state.issue_category == 'sizing' and st.session_state.sizing_fist_ref == False:
-    sample_image = Image.open("sizing/sample_sizing_image.jpg")
-    st.image(sample_image, caption="Sample Image", width=300)
-    sizing_img = st.file_uploader(label='Upload Image of your tshirt', type=['png', 'jpg'], key="img_file")
-    st.session_state.t_shirt_size = st.text_input("Please enter size  of your tshirt in Capital. Example: L")
-
-    if sizing_img:
-        sizing_img = Image.open(sizing_img)
-        sizing_img.save('sizing/sizing_img.jpg')
-
-    if st.button('Submit Image'):
-        if  st.session_state.t_shirt_size not in ('XS', 'S', 'M', 'L', 'XL', '2XL', '3XL', '4XL'):
-            st.write("Tshirt size is not entered properly. Please select between XS,S,M,L,XL,2XL,3XL,4XL ")
-        if sizing_img is None:
-            st.write("please upload an image")
-        predictions = model_json_prediction_for_sizing_issue('sizing/sizing_img.jpg')
-        predictions = get_corner_coordinates_for_tshirt(predictions)
-        corrected_predictions = correct_class_for_sleeves(predictions)
-        context = 'Here is the information available for a tshirt regarding its sizing. '
-        try:
-            CbyS, CbyL, SbyL = get_ratios_for_tshirt(corrected_predictions)
-            if t_shirt_size_chart_ratio[tshirt_size]['C/S'][0] <= CbyS <= t_shirt_size_chart_ratio[tshirt_size]['C/S'][1]:
-                context += "Chest by Shoulder ratio appears to be within range. "
-            elif CbyS < t_shirt_size_chart_ratio[tshirt_size]['C/S'][0]:
-                context += ("Chest by Shoulder ratio is less than expected. It means either Chest is of smaller size. "
-                            "or Shoulder is of larger size. ")
-            elif CbyS > t_shirt_size_chart_ratio[tshirt_size]['C/S'][1]:
-                context += ("Chest by Shoulder ratio is greater than expected. It means either Chest is "
-                            "of larger  size or Shoulder is of smaller size. ")
-
-            if t_shirt_size_chart_ratio[tshirt_size]['C/L'][0] <= CbyL <= t_shirt_size_chart_ratio[tshirt_size]['C/L'][1]:
-                context += "Chest by Tshirt length ratio appears to be within range. "
-            elif CbyL < t_shirt_size_chart_ratio[tshirt_size]['C/L'][0]:
-                context += ("Chest by Tshirt length ratio is less than expected. It means either Chest is of smaller "
-                            "size or Tshirt length is of larger size. ")
-            elif CbyL > t_shirt_size_chart_ratio[tshirt_size]['C/L'][1]:
-                context += ("Chest by Tshirt length ratio is greater than expected. It means either Chest is "
-                            "of larger  size or Tshirt length is of smaller size. ")
-
-            if t_shirt_size_chart_ratio[tshirt_size]['S/L'][0] <= SbyL <= t_shirt_size_chart_ratio[tshirt_size]['S/L'][1]:
-                context += "Shoulder by Tshirt length ratio appears to be within range. "
-            elif SbyL < t_shirt_size_chart_ratio[tshirt_size]['S/L'][0]:
-                context += (
-                    "Shoulder by Tshirt length ratio is less than expected. It means either Shoulder is of smaller "
-                    "size or Tshirt length is of larger size. ")
-            elif SbyL > t_shirt_size_chart_ratio[tshirt_size]['S/L'][1]:
-                context += ("Shoulder by Tshirt length ratio is greater than expected. It means either Shoulder is "
-                            "of larger  size or Tshirt length is of smaller size. ")
-
-            # st.write(context)
-            # st.write(st.session_state.generated_issue)
-
-            response = generate_response_based_upon_sizing_calculations(st.session_state.generated_issue, context)
-            st.session_state.sizing_fist_ref = True
-            response = response + (" However, if you want me to get precise calculation. I would like to "
-                                   "have multiple images of the same tshirt in a zip folder.Also I need to have "
-                                   "reference of any mobile to be placed on the image to be used as a reference ")
-            # st.write(response)
-            i = i + 1
-            message(response, key=i.__str__())
-        except Exception as e:
-            print(e.__str__())
 
 
 def submit(elseif=None):
