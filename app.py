7a46a55c737477dfae330cd75d446ed6fd0ef892
bhole <shrikant@flexli.in>
2023-11-25 19:13:48 +0530

    Sizing feature added. app tested for both sizing and defect

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from sizing.sizing_pre_processing import correct_class_for_sleeves, get_corner_c
 from quality.roboflow_inference import model_img_prediction, generate_response_based_upon_result, \
     get_iou_input_and_iou_predicted,  yolo_venkatesh, yolo_rahul
 
-st.header("Waistline v1.11")
+st.header("Waistline v1.12")
 # st.session_state.widget = ''
 i = 45
 
